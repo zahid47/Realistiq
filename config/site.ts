5e375b9f509ae45455811c49a94b16e24a6fafc3
zahid <epiczahid@gmail.com>
2023-06-10 20:54:33 +0600

    site config

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "Realistiq",
+  url: "https://realistiq.vercel.app",
+  //   ogImage: "",
+  description: "Your Path to Homeownership",
+  links: {
+    github: "https://github.com/zahid47/Realistiq",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
