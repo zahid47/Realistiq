15695956922dcd4987af1182d4e76abb5b82d36d
zahid <epiczahid@gmail.com>
2023-06-12 20:59:36 +0600

    up

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,7 +1,12 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function Home() {
   return (
-    <main>
-      main
+    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <Link className={buttonVariants({ variant: "default" })} href="/listings">
+        View listings
+      </Link>
     </main>
   );
 }
