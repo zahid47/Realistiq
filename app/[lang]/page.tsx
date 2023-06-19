7bf5632882f5ffd16f9a1604a3a33132335dcc4d
zahid <epiczahid@gmail.com>
2023-06-19 07:06:59 +0600

    update site description

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -7,13 +7,14 @@ export default async function Home() {
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-          <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-            Stress-Free <span className="text-deep-purple-600">Home</span> Renting
+          <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
+            Stress-Free <span className="text-deep-purple-600">Home</span>{" "}
+            Renting
           </h1>
           <h2 className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            Find your stress-free rental with Realistiq. Explore premium
-            listings, connect with trusted landlords, and experience renting
-            made easy. Start now for a seamless rental process.
+            Find stress-free rental with Realistiq. Explore premium listings,
+            connect with trusted landlords, and experience a hassle-free renting
+            process.
           </h2>
           <div className="space-x-4">
             <Link
--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,8 @@ export const siteConfig = {
   name: "Realistiq",
   url: "https://realistiq.vercel.app",
   //   ogImage: "",
-  description: "Stress-Free Home Renting",
+  description:
+    "Find stress-free rental with Realistiq. Explore premium listings, connect with trusted landlords, and experience a hassle-free renting process.",
   links: {
     github: "https://github.com/zahid47/Realistiq",
   },
