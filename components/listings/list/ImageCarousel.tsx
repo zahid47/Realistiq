a3b3fc77b070d623314b5a7a39e86bae85863afb
zahid <epiczahid@gmail.com>
2023-06-23 03:15:32 +0600

    image blur data

--- components/listings/list/ImageCarousel.tsx
+++ components/listings/list/ImageCarousel.tsx
@@ -1,25 +1,25 @@
 import Image from "next/image";
 import { Carousel } from "@material-tailwind/react";
-import ClientOnly from "../../providers/ClientOnly";
+import { getRGBDataURL } from "@/lib/utils";
 
 export default function ImageCarousel({ images }: { images: any[] }) {
   return (
-    <ClientOnly>
-      <Carousel
-        className="max-h-[14rem] min-h-[14rem] min-w-[14rem] max-w-[14rem] rounded-lg"
-        loop={true}
-      >
-        {images.map((image) => (
-          <Image
-            key={image.id}
-            className="h-[14rem] w-[14rem] rounded-lg object-cover" //FIXME: really bad hack
-            src={image.url}
-            alt={image.alt}
-            height={300}
-            width={300}
-          />
-        ))}
-      </Carousel>
-    </ClientOnly>
+    <Carousel
+      className="max-h-[14rem] min-h-[14rem] min-w-[14rem] max-w-[14rem] rounded-lg"
+      loop={true}
+    >
+      {images.map((image) => (
+        <Image
+          key={image.id}
+          className="h-[14rem] w-[14rem] rounded-lg object-cover" //FIXME: really bad hack
+          placeholder="blur"
+          blurDataURL={getRGBDataURL(209, 209, 209)} // grey
+          src={image.url}
+          alt={image.alt}
+          height={224}
+          width={224}
+        />
+      ))}
+    </Carousel>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -31,3 +31,18 @@ export function pluralized(countOrItems: number | Array<any>) {
 export function captitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export const getRGBDataURL = (r: number, g: number, b: number) => {
+  const keyStr =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+  const triplet = (e1: number, e2: number, e3: number) =>
+    keyStr.charAt(e1 >> 2) +
+    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+    keyStr.charAt(e3 & 63);
+
+  return `data:image/gif;base64,R0lGODlhAQABAPAA${
+    triplet(0, r, g) + triplet(b, 255, 255)
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+};
