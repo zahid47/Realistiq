8280fc7d84c59c5d80f0a194522975b7cac166d6
zahid <epiczahid@gmail.com>
2023-06-26 10:21:04 +0600

    update error pages

--- app/[lang]/error.tsx
+++ app/[lang]/error.tsx
@@ -1,5 +1,6 @@
-"use client"; // Error components must be Client Components
+"use client";
 
+import { Button } from "@mantine/core";
 import { useEffect } from "react";
 
 export default function Error({
@@ -10,21 +11,31 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="text-base font-semibold text-primary">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-secondary sm:text-5xl">
+          Something went wrong!
+        </h1>
+        <p className="mt-6 text-base leading-7 text-muted-foreground">
+          Sorry about that, please try again later.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Button
+            onClick={
+              // Attempt to recover by trying to re-render the segment
+              () => reset()
+            }
+            className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+          >
+            Go back home
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
