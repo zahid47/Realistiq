50579b0daa8a9246432e2d0225c0b3cde294ebe4
zahid <epiczahid@gmail.com>
2023-06-26 12:25:07 +0600

    update error pages

--- app/[lang]/error.tsx
+++ app/[lang]/error.tsx
@@ -17,8 +17,7 @@ export default function Error({
   return (
     <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-primary">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-secondary sm:text-5xl">
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Something went wrong!
         </h1>
         <p className="mt-6 text-base leading-7 text-muted-foreground">
@@ -32,7 +31,7 @@ export default function Error({
             }
             className="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
           >
-            Go back home
+            Try again
           </Button>
         </div>
       </div>
