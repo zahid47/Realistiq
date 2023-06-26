9f5d2bb3c80aaaf497ba7649d41cd4829be4d269
zahid <epiczahid@gmail.com>
2023-06-26 10:38:08 +0600

    update home page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,27 +4,65 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <>
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-          <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
-            Stress Free <span className="text-primary">Home</span> Renting
+    <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#fb923c] to-[#7c3aed] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          <div className="text-mute relative rounded-full px-3 py-1 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            We are open source.{" "}
+            <a
+              href="https://github.com/zahid47/Realistiq"
+              target="_blank"
+              className="font-semibold text-primary"
+            >
+              <span className="absolute inset-0" aria-hidden="true" />
+              Learn more <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Stress free <span className="text-primary">home</span> renting for
+            everyone
           </h1>
-          <h2 className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+          <p className="mt-6 text-lg leading-8 text-gray-600">
             Find stress-free rental with Realistiq. Explore premium listings,
             connect with trusted landlords, and experience a hassle-free renting
             process.
-          </h2>
-          <div className="space-x-4">
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
               href="/listings"
               className={cn(buttonVariants({ variant: "default", size: "lg" }))}
             >
-              <p className="text-lg">Browse Listings</p>
+              Browse Listings
             </Link>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+      <div
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-40rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#fb923c] to-[#7c3aed] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+    </div>
   );
 }
