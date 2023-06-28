9f3b5734d434235bb6a51136971c79fabec1d3e8
zahid <epiczahid@gmail.com>
2023-06-28 13:48:23 +0600

    update mobile loading screen for listings page

--- app/[lang]/listings/loading.tsx
+++ app/[lang]/listings/loading.tsx
@@ -4,10 +4,10 @@ import ListingsMapSkeleton from "@/components/skeletons/ListingsMapSkeleton";
 export default function loading() {
   return (
     <div className="flex flex-row">
-      <div className="h-[calc(100vh-4rem)] w-2/5">
+      <div className="h-[calc(100vh-4rem)] w-full lg:w-2/5">
         <ListingsListSkeleton />
       </div>
-      <div className="mb-2 mr-2 w-3/5 overflow-hidden rounded-lg">
+      <div className="mb-2 mr-2 hidden overflow-hidden rounded-lg lg:block lg:w-3/5">
         <ListingsMapSkeleton />
       </div>
     </div>
