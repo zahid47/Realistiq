775d920e825b85f3fd3ac550a66af88e1e369f51
zahid <epiczahid@gmail.com>
2023-07-15 21:25:27 +0600

    update property details page

--- components/listings/single-listing/SingleListing.tsx
+++ components/listings/single-listing/SingleListing.tsx
@@ -23,11 +23,20 @@ export default function SingleListing({ uuid }: Props) {
   const listing = listingDetailsQueryResult.data;
 
   return (
-    <div>
+    <div className="text-center">
       <p className="text-center text-sm text-slate-500">
         <i>[Work in progress]</i>
       </p>
-      Property for rent in <b>{listing.location.address}</b>
+      Property for rent in <b>{listing.location.address}.</b>
+      <p>
+        With a total area of <b>{listing.details.floor_area} SqFt</b>, this
+        property has <b>{listing.details.beds} beds</b> and{" "}
+        <b>{listing.details.baths} baths</b>. The price is{" "}
+        <b>
+          {listing.price.amount} {listing.price.currency}
+        </b>{" "}
+        per <b>{listing.price.interval.toLowerCase()}</b>.
+      </p>
     </div>
   );
   // return <pre>{JSON.stringify(listing, null, 2)}</pre>;
