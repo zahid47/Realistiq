4076620fa293c20f6d61fac8e4409aca494b4857
zahid <epiczahid@gmail.com>
2023-07-24 20:05:14 +0600

    responsive fixes

--- components/listings/create/CreateListingForm.tsx
+++ components/listings/create/CreateListingForm.tsx
@@ -9,14 +9,15 @@ import dynamic from "next/dynamic";
 import { useParams, useRouter } from "next/navigation";
 import { createListing } from "@/actions/api-calls/listing";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Stepper } from "@mantine/core";
+import { createStyles, Stepper } from "@mantine/core";
 import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
-import * as z from "zod";
+import { z } from "zod";
 import { toast } from "@/lib/hooks/use-toast";
 import { createListingSchema } from "@/lib/validators/listing";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
+import { Skeleton } from "@/components/ui/skeleton";
 import BedsBathsFloorAreaInput from "./BedsBathsFloorAreaInput";
 import DescriptionInput from "./DescriptionInput";
 import ImageInput from "./ImageInput";
@@ -24,30 +25,41 @@ import PriceInput from "./PriceInput";
 
 const LocationInput = dynamic(() => import("./LocationInput"), {
   ssr: false,
-  loading: () => <p>Loading map...</p>,
+  loading: () => <Skeleton className="h-96" />,
 });
 
 export default function CreateListingForm() {
+  const steps = [
+    ["address", "latitude", "longitude"],
+    ["beds", "baths", "floor_area"],
+    ["description"],
+    ["photos"],
+    ["amount", "interval"],
+  ] as const;
+
   const [active, setActive] = useState(0);
   const [uploading, setUploading] = useState(false);
+  const router = useRouter();
+  const { lang } = useParams();
+
+  const useStyles = createStyles((theme) => ({
+    steps: {
+      [theme.fn.smallerThan("xs")]: {
+        display: "none",
+      },
+    },
+  }));
 
   const nextStep = () =>
     setActive((current) => (current < 4 ? current + 1 : current));
   const prevStep = () =>
     setActive((current) => (current > 0 ? current - 1 : current));
 
-  const router = useRouter();
-  const { lang } = useParams();
-
-  const form = useForm<z.infer<typeof createListingSchema>>({
-    resolver: zodResolver(createListingSchema),
-    mode: "all",
-    defaultValues: {
-      beds: 1,
-      baths: 1,
-      interval: "MONTH",
-    },
-  });
+  const handleNextStep = () => {
+    form.trigger(steps[active]).then(async (isValid) => {
+      isValid && nextStep();
+    });
+  };
 
   const mutation = useMutation({
     mutationFn: (values: z.infer<typeof createListingSchema>) => {
@@ -65,17 +77,21 @@ export default function CreateListingForm() {
     },
   });
 
-  const steps = [
-    ["address", "latitude", "longitude"],
-    ["beds", "baths", "floor_area"],
-    ["description"],
-    ["photos"],
-    ["amount", "interval"],
-  ] as const;
+  const form = useForm<z.infer<typeof createListingSchema>>({
+    resolver: zodResolver(createListingSchema),
+    mode: "all",
+    defaultValues: {
+      beds: 1,
+      baths: 1,
+      interval: "MONTH",
+    },
+  });
+
+  const { classes } = useStyles();
 
   return (
     <div>
-      <div className="mx-auto max-w-2xl space-y-6 px-4 pt-12">
+      <div className="mx-auto max-w-2xl space-y-6 px-4 sm:pt-12">
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit((values) => mutation.mutate(values))}
@@ -86,8 +102,8 @@ export default function CreateListingForm() {
                 color="violet"
                 active={active}
                 onStepClick={setActive}
-                breakpoint="sm"
                 allowNextStepsSelect={false}
+                classNames={classes}
               >
                 <Stepper.Step>
                   <LocationInput form={form} />
@@ -114,7 +130,7 @@ export default function CreateListingForm() {
                 </Stepper.Step>
               </Stepper>
 
-              <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center gap-4 border-t-2 bg-white p-4">
+              <div className="sticky bottom-0 left-0 right-0 flex items-center justify-center gap-4 border-t-2 bg-white p-4">
                 <Button variant="outline" type="button" onClick={prevStep}>
                   Back
                 </Button>
@@ -122,11 +138,7 @@ export default function CreateListingForm() {
                   isLoading={mutation.isLoading || uploading}
                   disabled={mutation.isLoading || uploading}
                   type={active === steps.length - 1 ? "submit" : "button"}
-                  onClick={() => {
-                    form.trigger(steps[active]).then(async (isValid) => {
-                      isValid && nextStep();
-                    });
-                  }}
+                  onClick={handleNextStep}
                 >
                   {active === steps.length - 1 ? "Submit" : "Next"}
                 </Button>
