758248b4d9272f507180f0bce790d0444dd00694
zahid <epiczahid@gmail.com>
2023-07-29 14:19:21 +0600

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,38 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
+const generateDescription = () => {
+  return `<h2>Home for <em>rent </em>with a <strong><u>${faker.commerce
+    .productAdjective()
+    .toLowerCase()}</u></strong><u> </u>${faker.word.noun()}!</h2><p><i>(This is an auto generated demo listing)</i>
+ </p>
+ <p>
+   ${faker.lorem.paragraph({
+     min: 5,
+     max: 8,
+   })}
+ </p>
+ <p></p>
+ <h2><strong>${faker.lorem.words({ min: 2, max: 3 })}:</strong></h2>
+ <ol>
+    ${Array.from({
+      length: faker.number.int({
+        min: 5,
+        max: 12,
+      }),
+    })
+      .map(() => {
+        return `<li><p>${faker.lorem.sentence()}</p></li>`;
+      })
+      .join("")}
+ </ol>
+ <p></p>
+ <p>${faker.lorem.lines({
+   min: 2,
+   max: 4,
+ })}</p>`.replace(/\n/g, "");
+};
+
 async function main() {
   if (process.env.NODE_ENV === "production") {
     console.error("Refusing to seed non-development database");
@@ -13,8 +45,7 @@ async function main() {
   const user = await db.user.findFirst();
 
   if (!user) {
-    console.error("❌ No user found");
-    return;
+    throw new Error("❌ No user found");
   }
 
   console.log("Clearing existing data...");
@@ -29,13 +60,13 @@ async function main() {
 
   console.log("Seeding...");
 
-  const fakeListings = Array.from({ length: 200 }).map(() => {
+  const fakeListings = Array.from({ length: 50 }).map(() => {
     return {
       user_id: user.id,
       details: {
         create: {
-          description: `<p>${faker.lorem.paragraph()}</p>`,
-          beds: faker.number.int({ min: 1, max: 5 }),
+          description: generateDescription(),
+          beds: faker.number.int({ min: 1, max: 9 }),
           baths: faker.number.int({ min: 1, max: 5 }),
           floor_area: faker.number.int({ min: 20, max: 200 }),
         },
@@ -55,13 +86,16 @@ async function main() {
             min: -121,
             max: -76,
           }),
-          address: `${faker.location.streetAddress()}, ${faker.location.city()}, ${faker.location.state(
-            { abbreviated: true }
-          )} ${faker.location.zipCode()}`,
+          address: `${faker.location.city()}, ${faker.location.streetAddress()}, United States`,
         },
       },
       photos: {
-        create: Array.from({ length: 4 }).map(() => {
+        create: Array.from({
+          length: faker.number.int({
+            min: 6,
+            max: 20,
+          }),
+        }).map(() => {
           return {
             url: faker.image.urlPicsumPhotos(),
             alt: faker.lorem.sentence(),
